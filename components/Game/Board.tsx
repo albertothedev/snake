f2f1c6029b9a925f0ce8a1edc6bd70013970df46
Alberto Vilches <albertothedev@protonmail.com>
2022-09-02 15:49:38 +0200

    refactor(cloud): separate JSX into components
    resolves #28

---
+++ components/Game/Board.tsx
@@ -0,0 +1,212 @@
+import { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+
+import useInterval from "utils/useInterval";
+import { setGameState, setPlayerBest, setScore } from "redux/index";
+
+const gameSpeed: number = 150;
+const boardCells: number = 1000;
+
+const generateBoard = () => {
+  const board = [];
+
+  for (let i = 0; i < boardCells; i++)
+    board.push({
+      id: `cell-${i}`,
+      position: i,
+      hasFood: false,
+      hasSnake: false,
+    });
+
+  board[Math.floor(Math.random() * board.length)].hasFood = true;
+
+  return board;
+};
+
+export default function Board() {
+  const dispatch = useDispatch();
+
+  const [direction, _setDirection] = useState<{
+    current: "up" | "right" | "down" | "left" | null;
+    new: "up" | "right" | "down" | "left";
+  }>({
+    current: null,
+    new: "down",
+  });
+  const directionRef = useRef(direction);
+  const setDirection = (data: {
+    current: "up" | "right" | "down" | "left" | null;
+    new: "up" | "right" | "down" | "left";
+  }) => {
+    directionRef.current = data;
+    _setDirection(data);
+  };
+  const boardDimensions: { rows: number; columns: number } = {
+    rows: 25,
+    columns: 40,
+  };
+  const [board, setBoard] = useState<
+    Array<{ id: string; position: number; hasFood: boolean; hasSnake: boolean }>
+  >(generateBoard());
+  const [snake, setSnake] = useState<{
+    head: number;
+    body: Array<number>;
+    length: number;
+  }>({
+    head: 0,
+    body: [],
+    length: 5,
+  });
+
+  useEffect(() => {
+    if (
+      Number(localStorage.getItem("playerBest") as string) <
+      snake.length - 5
+    ) {
+      localStorage.setItem("playerBest", (snake.length - 5).toString());
+      dispatch(setPlayerBest(snake.length - 5));
+    }
+  }, [snake.length, dispatch]);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => keyDown(e), false);
+
+    const keyDown = (e: KeyboardEvent): void => {
+      if (
+        (e.key === "w" || e.key === "ArrowUp") &&
+        directionRef.current.new !== "down" &&
+        directionRef.current.new !== "up"
+      )
+        setDirection({
+          current: directionRef.current.new,
+          new: "up",
+        });
+      else if (
+        (e.key === "s" || e.key === "ArrowDown") &&
+        directionRef.current.new !== "up" &&
+        directionRef.current.new !== "down"
+      )
+        setDirection({
+          current: directionRef.current.new,
+          new: "down",
+        });
+      else if (
+        (e.key === "a" || e.key === "ArrowLeft") &&
+        directionRef.current.new !== "right" &&
+        directionRef.current.new !== "left"
+      )
+        setDirection({
+          current: directionRef.current.new,
+          new: "left",
+        });
+      else if (
+        (e.key === "d" || e.key === "ArrowRight") &&
+        directionRef.current.new !== "left" &&
+        directionRef.current.new !== "right"
+      )
+        setDirection({
+          current: directionRef.current.new,
+          new: "right",
+        });
+    };
+  }, []);
+
+  useInterval(() => {
+    moveSnake();
+  }, gameSpeed);
+
+  const moveSnake = (): void => {
+    let snakeTemp = { ...snake };
+    let boardTemp = [...board];
+
+    snakeTemp.body.unshift(snakeTemp.head);
+
+    switch (directionRef.current.new) {
+      case "up":
+        if (snakeTemp.head < boardDimensions.columns)
+          return dispatch(setGameState("over"));
+
+        snakeTemp.head -= boardDimensions.columns;
+        break;
+
+      case "right":
+        if (((snakeTemp.head + 1) / boardDimensions.columns) % 1 === 0)
+          return dispatch(setGameState("over"));
+
+        snakeTemp.head += 1;
+        break;
+
+      case "down":
+        if (
+          snakeTemp.head >=
+          boardDimensions.rows * boardDimensions.columns -
+            boardDimensions.columns
+        )
+          return dispatch(setGameState("over"));
+
+        snakeTemp.head += boardDimensions.columns;
+        break;
+
+      case "left":
+        if (
+          ((snakeTemp.head + boardDimensions.columns) /
+            boardDimensions.columns) %
+            1 ===
+          0
+        )
+          return dispatch(setGameState("over"));
+
+        snakeTemp.head -= 1;
+        break;
+    }
+
+    if (snakeTemp.body.length >= snakeTemp.length) {
+      const position: number = snakeTemp.body.pop()!;
+      boardTemp[position].hasSnake = false;
+    }
+
+    snakeTemp.body.forEach((position: number) => {
+      if (position === snakeTemp.head) return dispatch(setGameState("over"));
+    });
+
+    if (boardTemp[snakeTemp.head].hasFood === true) {
+      boardTemp[snakeTemp.head].hasFood = false;
+
+      snakeTemp.length += 5;
+
+      let randomCell = boardTemp[Math.floor(Math.random() * boardTemp.length)];
+
+      while (randomCell.hasSnake)
+        randomCell = boardTemp[Math.floor(Math.random() * boardTemp.length)];
+
+      randomCell.hasFood = true;
+    }
+
+    [snakeTemp.head, snakeTemp.body]
+      .flat()
+      .forEach((position: number) => (boardTemp[position].hasSnake = true));
+
+    setBoard(boardTemp);
+    setSnake(snakeTemp);
+    dispatch(setScore(snakeTemp.length - 5));
+  };
+
+  return (
+    <>
+      {board.map((cell, index) => (
+        <div
+          className={`game__board__cell${
+            cell.hasSnake ? " game__board__cell--snake" : ""
+          } ${cell.hasFood ? " game__board__cell--food" : ""}`}
+          id={cell.id}
+          key={index}
+        >
+          {cell.hasFood &&
+            Array.from(Array(9), (e, i) => (
+              <div className={`${i % 2 !== 0 ? "dark" : ""}`} key={i}></div>
+            ))}
+        </div>
+      ))}
+    </>
+  );
+}
---
+++ components/Game/Loss.tsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import axios, { type AxiosError } from "axios";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  setDataFetched,
+  setGameState,
+  setRequestType,
+  RootState,
+} from "redux/index";
+
+export default function Loss() {
+  const dispatch = useDispatch();
+
+  const { score } = useSelector((state: RootState) => state);
+
+  const [submitBlinking, setSubmitBlinking] = useState<boolean>(true);
+  const [restartBlinking, setRestartBlinking] = useState<boolean>(true);
+  const [value, setValue] = useState<string>("");
+
+  useEffect((): void => {
+    if (localStorage.getItem("username"))
+      setValue(localStorage.getItem("username") as string);
+  }, []);
+
+  const submit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (score >= 5 && value !== "") {
+      const data = {
+        username: value,
+        score: score,
+        date: new Date().toJSON().slice(0, -5),
+      };
+
+      axios
+        .post(`/api/addPlayer`, {
+          data,
+        })
+        .then(() => {
+          dispatch(setRequestType("day"));
+          dispatch(setDataFetched(false));
+
+          dispatch(setGameState("waiting"));
+        })
+        .catch((error: AxiosError) => console.error(error));
+
+      localStorage.setItem("username", value);
+    }
+  };
+
+  return (
+    <div className="game__board__loss">
+      <h1 className="game__board__loss__title">YOU LOST!</h1>
+      {score >= 5 ? (
+        <h2 className="game__board__loss__info">
+          ENTER YOUR NAME AND SUBMIT YOUR SCORE FOR OTHERS TO SEE
+        </h2>
+      ) : (
+        <h2 className="game__board__loss__info">
+          DO BETTER TO SUBMIT YOUR SCORE
+        </h2>
+      )}
+
+      <form onSubmit={submit} className="game__board__loss__submitForm">
+        {score >= 5 && (
+          <input
+            type="text"
+            maxLength={10}
+            value={value}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setValue(e.target.value.toUpperCase())
+            }
+            className="game__board__loss__submitForm__usernameInput"
+            name="username"
+            required
+          />
+        )}
+
+        {score >= 5 && (
+          <button
+            type="submit"
+            className={`game__board__loss__submitForm__submit ${
+              submitBlinking
+                ? "game__board__loss__submitForm__submit--blink"
+                : ""
+            }`}
+            onMouseOver={() => setSubmitBlinking(false)}
+            onMouseOut={() => setSubmitBlinking(true)}
+          >
+            SUBMIT
+          </button>
+        )}
+
+        <button
+          className={`game__board__loss__submitForm__restart ${
+            restartBlinking
+              ? "game__board__loss__submitForm__restart--blink"
+              : ""
+          }`}
+          onClick={() => dispatch(setGameState("playing"))}
+          onMouseOver={() => setRestartBlinking(false)}
+          onMouseOut={() => setRestartBlinking(true)}
+          type="button"
+        >
+          PLAY AGAIN
+        </button>
+      </form>
+    </div>
+  );
+}
